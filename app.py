5897acdd2f309322f294c25917e7238c1c66be33
HP <rawan.manswor@gmail.com>
2023-12-26 17:00:01 +0300

    db config

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 chatbot = SaudiPersonalLawAssistant()
 app.config['SECRET_KEY']=""
 
-app.config['MYSQL_HOST'] = os.getenv()
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "law"
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app) 
 # Inside your Flask route or function where you're using MySQL functionality
